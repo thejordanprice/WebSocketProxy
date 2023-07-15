b33b9c40cfc8e2f9cc28040f71a4ba6523ee0828
Jordan Price <thejordanprice@gmx.com>
2023-07-14 21:50:06 -0400

    Update WebSocketProxy.js

--- WebSocketProxy.js
+++ WebSocketProxy.js
@@ -14,7 +14,9 @@
       const originalSend = ws.send.bind(ws);
       ws.send = function (data) {
         console.log('Intercepted outgoing message:', data);
-        return originalSend(data);
+        if (originalSend) {
+          return originalSend(data);
+        }
       };
 
       const originalOnMessage = ws.onmessage;
@@ -22,6 +24,46 @@
         console.log('Intercepted incoming message:', event.data);
         if (originalOnMessage) {
           return originalOnMessage(event);
+        }
+      };
+
+      const originalClose = ws.close.bind(ws);
+      ws.close = function () {
+        console.log('Intercepted close event');
+        if (originalClose) {
+          return originalClose();
+        }
+      };
+
+      const originalAddEventListener = ws.addEventListener.bind(ws);
+      ws.addEventListener = function (eventName, listener, options) {
+        console.log('Intercepted addEventListener:', eventName);
+        if (originalAddEventListener) {
+          return originalAddEventListener(eventName, listener, options);
+        }
+      };
+
+      const originalRemoveEventListener = ws.removeEventListener.bind(ws);
+      ws.removeEventListener = function (eventName, listener, options) {
+        console.log('Intercepted removeEventListener:', eventName);
+        if (originalRemoveEventListener) {
+          return originalRemoveEventListener(eventName, listener, options);
+        }
+      };
+
+      const originalOnOpen = ws.onopen;
+      ws.onopen = function (event) {
+        console.log('Intercepted open event');
+        if (originalOnOpen) {
+          return originalOnOpen(event);
+        }
+      };
+
+      const originalOnClose = ws.onclose;
+      ws.onclose = function (event) {
+        console.log('Intercepted close event');
+        if (originalOnClose) {
+          return originalOnClose(event);
         }
       };
 
